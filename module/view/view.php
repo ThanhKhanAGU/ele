053218393c19fd0706b4f027e0a439056b376e30
ThanhKhan <thanhkhan.dongbo.saoluu@gmail.com>
2021-10-10 19:12:56 +0700

    fix comment

--- module/view/view.php
+++ module/view/view.php
@@ -1,42 +1,39 @@
 <?php
-if(is_file("function/function.view.php"))require_once("function/function.view.php");
-function view($url,$arr = [])
+if (is_file("function/function.view.php")) require_once("function/function.view.php");
+function view($url, $arr = [], $mess = [])
 {
-    if($url == 'message') $url = '../../module/error/index.html';
-    $url = str_replace('\\','/',$url);
-    $url = "app/view/".$url;
+    if ($url == 'message') $url = '../../module/error/index.html';
+    $url = str_replace('\\', '/', $url);
+    $url = "app/view/" . $url;
     $myfile = fopen($url, "r") or die("View Not Found");
     $data = fread($myfile, filesize($url));
     fclose($myfile);
-    
-    if(strpos($data, '@extend') !== false)// Xử lý kế thừa
-    { 
+
+    if (strpos($data, '@extend') !== false) // Xử lý kế thừa
+    {
         $url_con = preg_replace('/@extend\(|\)(.|\s)+/', '', trim($data));
-        $myfile = fopen('app/view/'.$url_con, "r") or die("View Not Found");
-        $data_extend = fread($myfile, filesize('app/view/'.$url_con));
+        $myfile = fopen('app/view/' . $url_con, "r") or die("View Not Found");
+        $data_extend = fread($myfile, filesize('app/view/' . $url_con));
         fclose($myfile);
         preg_match_all('/@paste\((.)+\)/', $data_extend, $output_array);
         foreach ($output_array[0] as $key_item) {
-            $key = str_replace(['@paste(',')'],'', $key_item);
-            $value = explode("@copy($key)",$data);
-            if(count($value)==1) {
-                 $data_extend = str_replace("@paste($key)",'',$data_extend);
-            }
-            else {
+            $key = str_replace(['@paste(', ')'], '', $key_item);
+            $value = explode("@copy($key)", $data);
+            if (count($value) == 1) {
+                $data_extend = str_replace("@paste($key)", '', $data_extend);
+            } else {
                 $value = explode("@endcopy", $value[1])[0];
                 $data_extend = str_replace("@paste($key)", trim($value), $data_extend);
             }
         }
         $data = $data_extend;
-    }
-    else
-    {
+    } else {
         $data = preg_replace('/@paste\((.|\b)+\)/', '', $data);
     }
 
     // bắt đầu xử lý vòng lặp
     preg_match_all('/@foreach\((.|\b)+\)/', $data, $output_array);
-    foreach($output_array[0] as $item){
+    foreach ($output_array[0] as $item) {
         $value = preg_replace('/(@(\w|\d)+\()|(\)\z)/', '', trim($item));
         $function = "foreach";
 
@@ -59,7 +56,7 @@ function view($url,$arr = [])
         $data = str_replace("@$function($value)", "<?php $function($value){?>", $data);
     }
 
-    $data = str_replace(["@endforeach", "@endfor","@endwhile","@endif","@end"],"<?php } ?>",$data);
+    $data = str_replace(["@endforeach", "@endfor", "@endwhile", "@endif", "@end"], "<?php } ?>", $data);
     //end xử lý vòng lặp
 
     //xử lý if elseif else
@@ -84,21 +81,23 @@ function view($url,$arr = [])
         $value = preg_replace('/(@(\w|\d)+\()|(\)\z)/', '', trim($item));
         $function = preg_replace('/\A@|\((.|\b)+\)/', '', trim($item));
 
-        $data = str_replace("@$function($value)","<?php print_r($function($value));?>", $data);
+        $data = str_replace("@$function($value)", "<?php print_r($function($value));?>", $data);
     }
 
-    $data = str_replace("@else",'<?php }else{ ?>',$data);
+    $data = str_replace("@else", '<?php }else{ ?>', $data);
     //end xử lý if elseif else
 
-    $data = str_replace(["{{","}}","{!","!}"], ["<?php print_r(",");?>","<?php ","?>"], $data);
-    
+    $data = str_replace(["{{", "}}", "{!", "!}"], ["<?php print_r(", ");?>", "<?php ", "?>"], $data);
+
     $value = "";
-    foreach($arr as $key => $item)
-    {
-        $value.=" \$$key = \$arr['$key'] ?? null;";
+    foreach ($arr as $key => $item) {
+        $value .= " \$$key = \$arr['$key'] ?? null;";
+    }
+    foreach ($mess as $key => $item) {
+        $value .= " \$$key = \$arr['$key'] ?? null;";
     }
     $value .= "?>";
-    $data = $value.$data;
+    $data = $value . $data;
     eval($data);
 }
 
